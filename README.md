20d2f7bd44c70498589c1d19ca37d938e87e8550
Kellen Kolbeck & Andrew Brennwald <student@epicodus.com>
2015-08-04 11:29:38 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+#Scrabble Score
+
+##### 
---
+++ spec/scrabble_score_spec.rb
@@ -0,0 +1,14 @@
+# Test specs for scrabble_score method
+# Authors: Andrew Brennwald and Kellen Kolbeck
+
+require('rspec')
+require('scrabble_score')
+
+describe('String#scrabble_score') do
+
+# TEST 1: Shows the scrabble score for a given letter
+
+  it ("Shows the scrabble score for a given letter") do
+    expect("a".scrabble_score()).to(eq.(1))
+  end
+end
