f0fdec2ce0957e6ec5bc88990ce466263f44c70a
Kellen Kolbeck & Andrew Brennwald <student@epicodus.com>
2015-08-04 12:24:04 -0700

    added working code for first spec

---
+++ lib/scrabble_score.rb
@@ -0,0 +1,16 @@
+
+class String
+  define_method(:scrabble_score) do
+    scrabble_scoreboard = 0
+    scrabble_holder = self.split("")
+    one_pt_letters = ["a"]
+    scrabble_holder.each() do |foo|
+      if one_pt_letters.include?(foo)
+        scrabble_scoreboard = scrabble_scoreboard + 1
+      else
+        scrabble_scoreboard
+      end
+    end
+    scrabble_scoreboard
+  end
+end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -8,7 +8,7 @@ describe('String#scrabble_score') do
 
 # TEST 1: Shows the scrabble score for a given letter
 
-  it ("Shows the scrabble score for a given letter") do
-    expect("a".scrabble_score()).to(eq.(1))
+  it("Shows the scrabble score for a given letter") do
+    expect("a".scrabble_score()).to(eq(1))
   end
 end
