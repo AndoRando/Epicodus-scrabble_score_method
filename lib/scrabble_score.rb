73f91b72b55e03ea09096803d2a49c8586ff33c5
Kellen Kolbeck & Andrew Brennwald <student@epicodus.com>
2015-08-04 14:04:12 -0700

    Adding working code to handle mixed-case inputs

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -2,16 +2,17 @@
 class String
   define_method(:scrabble_score) do
     scrabble_scoreboard = 0
-    scrabble_holder = self.split("")
-    scrabble_holder.each() do |foo|
+    scrabble_holder = self.downcase().split("")
+    #scrabble_holder = scrabble_holder.downcase()
+    scrabble_holder.each() do |letter|
       one_pt_letters     = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
       two_pt_letters     = ["d", "g"]
       three_pt_letters   = ["b", "c", "m", "p"]
-      if one_pt_letters.include?(foo)
+      if one_pt_letters.include?(letter)
         scrabble_scoreboard = scrabble_scoreboard + 1
-      elsif two_pt_letters.include?(foo)
+      elsif two_pt_letters.include?(letter)
         scrabble_scoreboard = scrabble_scoreboard + 2
-      elsif three_pt_letters.include?(foo)
+      elsif three_pt_letters.include?(letter)
         scrabble_scoreboard = scrabble_scoreboard + 3
       else
         scrabble_scoreboard
