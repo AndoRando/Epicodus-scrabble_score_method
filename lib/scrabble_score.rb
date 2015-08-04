1dd38551a8213289d00b7ca164d1bf81c0c5dcac
Kellen Kolbeck & Andrew Brennwald <student@epicodus.com>
2015-08-04 13:31:11 -0700

    added working code for second spec

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -3,7 +3,7 @@ class String
   define_method(:scrabble_score) do
     scrabble_scoreboard = 0
     scrabble_holder = self.split("")
-    one_pt_letters = ["a"]
+    one_pt_letters = ["l", "a", "s", "t"]
     scrabble_holder.each() do |foo|
       if one_pt_letters.include?(foo)
         scrabble_scoreboard = scrabble_scoreboard + 1
