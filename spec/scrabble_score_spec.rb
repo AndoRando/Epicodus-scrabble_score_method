294a9ecf007b085f9b03298dd55281a5bcd1c662
Kellen Kolbeck & Andrew Brennwald <student@epicodus.com>
2015-08-04 12:26:17 -0700

    added second spec

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -11,4 +11,9 @@ describe('String#scrabble_score') do
   it("Shows the scrabble score for a given letter") do
     expect("a".scrabble_score()).to(eq(1))
   end
+
+# TEST 2: Shows the combined scrabble score for a given word
+  it("Shows the combined scrabble score for a given word") do
+    expect("last".scrabble_score()).to(eq(4))
+  end
 end
